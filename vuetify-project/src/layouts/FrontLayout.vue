822011d83be9ab13ac713379dc0613283e6c9f8b
kkon2075 <lone8877@gmail.com>
2023-01-03 23:33:30 +0800

    前後端應用

---
+++ vuetify-project/src/layouts/FrontLayout.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+v-app-bar(color="primary")
+  v-app-bar-title 購物網
+  v-spacer
+  v-btn(icon="mdi-account-plus" variant="text" to="/register")
+v-main
+  v-container
+    router-view
+</template>
